6f3dc0adf60ce1864b8188d7e421421d2b89c4aa
Immortal <45043776+mixamus13@users.noreply.github.com>
2022-02-27 14:51:34 +0300

    Added check annotation for method one arg

--- hw10-byteCodes/src/main/java/ru/otus/homework/proxy/DynamicInvocationHandler.java
+++ hw10-byteCodes/src/main/java/ru/otus/homework/proxy/DynamicInvocationHandler.java
@@ -1,20 +1,30 @@
 package ru.otus.homework.proxy;
 
-import ru.otus.homework.domain.TestLogging;
+import ru.otus.homework.annotation.Log;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DynamicInvocationHandler implements InvocationHandler {
 
-    private final TestLogging testLogging;
+    private final Object original;
+    private final Set<Method> logging;
 
-    public DynamicInvocationHandler(TestLogging testLogging) {
-        this.testLogging = testLogging;
+    public DynamicInvocationHandler(Object original) {
+        this.original = original;
+        logging = new HashSet<>();
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return method.invoke(testLogging, args);
+        Method originalMethod = original.getClass().getMethod(method.getName(), method.getParameterTypes());
+        if (originalMethod.isAnnotationPresent(Log.class)) {
+            System.out.print(method.getName() + " : ");
+            System.out.println(Arrays.toString(args));
+        }
+        return method.invoke(original, args);
     }
 }
